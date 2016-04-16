01d16d9544ac3f749c811d6712b1d55b11cfda95
savelichalex <savelichalex93@gmail.com>
2016-04-17 01:41:42 +0300

    Create add task screen with actions

--- src/ios/containers/App.js
+++ src/ios/containers/App.js
@@ -14,6 +14,7 @@ import { connect } from 'react-redux';
 import { combineReducers, bindActionCreators } from 'redux';
 import Launch from '../components/Launch';
 import Home from '../components/Home';
+import CreateTask from '../components/CreateTask';
 import * as tasksActions from '../actions/homeActions';
 
 const mapStateToProps = state => ({
@@ -34,9 +35,10 @@ const defaultSchema = {
 class App extends Component {
 	render() {
 		return (
-			<Router {...this.props} initial="launch">
+			<Router {...this.props} initial="createTask">
 				<Schema name="default" {...defaultSchema} />
 				<Route name="launch" component={Launch} hideNavBar={true} />
+				<Route name="createTask" component={CreateTask} hideNavBar={true}  />
 				<Route name="home" component={Home} hideNavBar={true}  />
 			</Router>
 		);
